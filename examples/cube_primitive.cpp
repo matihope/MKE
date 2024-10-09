aea127b0750befc6509a7c9deae8c2f11433d467
Mateusz Kolpa <mateusz.kolpa@wp.pl>
2024-10-09 17:16:07 +0200

    Change cube data

--- examples/cube_primitive.cpp
+++ examples/cube_primitive.cpp
@@ -1,3 +1,4 @@
+#include "MKE/Color.hpp"
 #include "MKE/DrawContext.hpp"
 #include "MKE/Input.hpp"
 #include "MKE/Math/MatrixUtils.hpp"
@@ -12,31 +13,50 @@ int main() {
 
 	auto [width, height] = window.getSize().bind();
 	const mk::math::Matrix4f camera_projection
-		= mk::math::perspective(45.f, float(width) / height, 1.f, 10000.f);
+		= mk::math::perspective(45.f, float(width) / height, 1.f, 100.f);
 
 	std::array vertices = {
-		mk::Vertex3D({ 0.0f, 0.0f, 0.0f }, { 1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }),
-		mk::Vertex3D({ 0.0f, 1.0f, 0.0f }, { 0.0f, 1.0f, 0.0f }, { 1.0f, 0.0f }),
-		mk::Vertex3D({ 1.0f, 1.0f, 0.0f }, { 0.0f, 1.0f, 1.0f }, { 0.0f, 0.0f }),
-		mk::Vertex3D({ 1.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 1.0f }, { 0.0f, 1.0f }),
-		mk::Vertex3D({ 0.0f, 0.0f, 1.0f }, { 1.0f, 0.0f, 0.0f }, { 0.0f, 1.0f }),
-		mk::Vertex3D({ 0.0f, 1.0f, 1.0f }, { 0.0f, 1.0f, 0.0f }, { 0.0f, 0.0f }),
-		mk::Vertex3D({ 1.0f, 1.0f, 1.0f }, { 0.0f, 1.0f, 1.0f }, { 1.0f, 0.0f }),
-		mk::Vertex3D({ 1.0f, 0.0f, 1.0f }, { 0.0f, 0.0f, 1.0f }, { 1.0f, 1.0f }),
-	};
-	std::array indices = {
-		0u, 1u, 2u, 0u, 2u, 3u,  // FRONT
-		// 0u, 1u, 4u, 1u, 4u, 5u,  // FRONT
-		1u, 2u, 5u, 2u, 5u, 6u,  //
-		2u, 3u, 6u, 3u, 6u, 7u,  //
-		3u, 0u, 7u, 0u, 7u, 4u,  //
-		4u, 5u, 6u, 4u, 6u, 7u,  // BEHIND
+		mk::Vertex3D({ -0.5f, -0.5f, -0.5f }, { 1.f, 0.f, 0.f }, { 0.0f, 0.0f }),
+		mk::Vertex3D({ 0.5f, -0.5f, -0.5f }, { 0.f, 1.f, 0.f }, { 1.0f, 0.0f }),
+		mk::Vertex3D({ 0.5f, 0.5f, -0.5f }, { 0.f, 0.f, 1.f }, { 1.0f, 1.0f }),
+		mk::Vertex3D({ 0.5f, 0.5f, -0.5f }, { 1.f, 0.f, 0.f }, { 1.0f, 1.0f }),
+		mk::Vertex3D({ -0.5f, 0.5f, -0.5f }, { 0.f, 1.f, 0.f }, { 0.0f, 1.0f }),
+		mk::Vertex3D({ -0.5f, -0.5f, -0.5f }, { 0.f, 0.f, 1.f }, { 0.0f, 0.0f }),
+		mk::Vertex3D({ -0.5f, -0.5f, 0.5f }, { 1.f, 0.f, 0.f }, { 0.0f, 0.0f }),
+		mk::Vertex3D({ 0.5f, -0.5f, 0.5f }, { 0.f, 1.f, 0.f }, { 1.0f, 0.0f }),
+		mk::Vertex3D({ 0.5f, 0.5f, 0.5f }, { 0.f, 1.f, 1.f }, { 1.0f, 1.0f }),
+		mk::Vertex3D({ 0.5f, 0.5f, 0.5f }, { 1.f, 0.f, 0.f }, { 1.0f, 1.0f }),
+		mk::Vertex3D({ -0.5f, 0.5f, 0.5f }, { 0.f, 1.f, 0.f }, { 0.0f, 1.0f }),
+		mk::Vertex3D({ -0.5f, -0.5f, 0.5f }, { 0.f, 0.f, 1.f }, { 0.0f, 0.0f }),
+		mk::Vertex3D({ -0.5f, 0.5f, 0.5f }, { 1.f, 0.f, 0.f }, { 1.0f, 0.0f }),
+		mk::Vertex3D({ -0.5f, 0.5f, -0.5f }, { 0.f, 1.f, 0.f }, { 1.0f, 1.0f }),
+		mk::Vertex3D({ -0.5f, -0.5f, -0.5f }, { 0.f, 0.f, 1.f }, { 0.0f, 1.0f }),
+		mk::Vertex3D({ -0.5f, -0.5f, -0.5f }, { 1.f, 0.f, 0.f }, { 0.0f, 1.0f }),
+		mk::Vertex3D({ -0.5f, -0.5f, 0.5f }, { 0.f, 1.f, 0.f }, { 0.0f, 0.0f }),
+		mk::Vertex3D({ -0.5f, 0.5f, 0.5f }, { 0.f, 0.f, 1.f }, { 1.0f, 0.0f }),
+		mk::Vertex3D({ 0.5f, 0.5f, 0.5f }, { 1.f, 0.f, 0.f }, { 1.0f, 0.0f }),
+		mk::Vertex3D({ 0.5f, 0.5f, -0.5f }, { 0.f, 1.f, 0.f }, { 1.0f, 1.0f }),
+		mk::Vertex3D({ 0.5f, -0.5f, -0.5f }, { 1.f, 0.f, 1.f }, { 0.0f, 1.0f }),
+		mk::Vertex3D({ 0.5f, -0.5f, -0.5f }, { 1.f, 0.f, 0.f }, { 0.0f, 1.0f }),
+		mk::Vertex3D({ 0.5f, -0.5f, 0.5f }, { 0.f, 1.f, 0.f }, { 0.0f, 0.0f }),
+		mk::Vertex3D({ 0.5f, 0.5f, 0.5f }, { 0.f, 0.f, 1.f }, { 1.0f, 0.0f }),
+		mk::Vertex3D({ -0.5f, -0.5f, -0.5f }, { 1.f, 0.f, 0.f }, { 0.0f, 1.0f }),
+		mk::Vertex3D({ 0.5f, -0.5f, -0.5f }, { 0.f, 1.f, 0.f }, { 1.0f, 1.0f }),
+		mk::Vertex3D({ 0.5f, -0.5f, 0.5f }, { 0.f, 0.f, 1.f }, { 1.0f, 0.0f }),
+		mk::Vertex3D({ 0.5f, -0.5f, 0.5f }, { 1.f, 0.f, 0.f }, { 1.0f, 0.0f }),
+		mk::Vertex3D({ -0.5f, -0.5f, 0.5f }, { 0.f, 1.f, 0.f }, { 0.0f, 0.0f }),
+		mk::Vertex3D({ -0.5f, -0.5f, -0.5f }, { 0.f, 0.f, 1.f }, { 0.0f, 1.0f }),
+		mk::Vertex3D({ -0.5f, 0.5f, -0.5f }, { 1.f, 0.f, 0.f }, { 0.0f, 1.0f }),
+		mk::Vertex3D({ 0.5f, 0.5f, -0.5f }, { 0.f, 1.f, 0.f }, { 1.0f, 1.0f }),
+		mk::Vertex3D({ 0.5f, 0.5f, 0.5f }, { 0.f, 0.f, 1.f }, { 1.0f, 0.0f }),
+		mk::Vertex3D({ 0.5f, 0.5f, 0.5f }, { 1.f, 0.f, 0.f }, { 1.0f, 0.0f }),
+		mk::Vertex3D({ -0.5f, 0.5f, 0.5f }, { 0.f, 1.f, 0.f }, { 0.0f, 0.0f }),
+		mk::Vertex3D({ -0.5f, 0.5f, -0.5f }, { 0.f, 0.f, 1.f }, { 0.0f, 1.0 }),
 	};
 
-	mk::VertexArray3D cube(true);
-	cube.setSize(8);
+	mk::VertexArray3D cube(false);
+	cube.setSize(36);
 	cube.setVertexBuffer(vertices.data(), vertices.size());
-	cube.setIndexBuffer(indices.data(), indices.size());
 	cube.save();
 
 	mk::Texture texture;
@@ -47,26 +67,13 @@ int main() {
 	context.camera  = camera_projection;
 	context.texture = &texture;
 
-	class Transform: public mk::Transformable {
-	public:
-		Transform() = default;
-	};
-
-	Transform position;
-	position.move(mk::math::Vector3f(-.5f));
-
 	float time = 0.;
 	while (!window.isExitRequested()) {
-		const float radius = 5.0f;
+		time += 1.f / 60.f;
+		float radius = 5.f;
 
 		float camX = sin(time) * radius;
 		float camZ = cos(time) * radius;
-
-		time += (window.isKeyPressed(mk::input::KEY::ARROW_RIGHT)
-		         - window.isKeyPressed(mk::input::KEY::ARROW_LEFT))
-		      * 0.01f;
-
-		context.transform = position.getTransform();
 
 		context.camera = camera_projection
 		               * mk::math::lookAt(
@@ -74,7 +81,6 @@ int main() {
 							 mk::math::Vector3f(0.0f),
 							 mk::math::Vector3f(0.f, 1.f, 0.f)
 					   );
-
 
 		window.clear(mk::Colors::DARK);
 		window.render3d(cube, context);
